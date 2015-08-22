481b899bd367ec16b3b979514eb1a0d57de91fcb
Saulo Medeiros de Araujo <saulo2@gmail.com>
2015-08-22 04:05:38 -0300

    Input mask added

--- client/src/main/typescript/client.ts
+++ client/src/main/typescript/client.ts
@@ -3,7 +3,7 @@
 /// <reference path="../../../typings/moment/moment.d.ts"/>
 /// <reference path="../../../../server/api/src/main/typescript/api.d.ts"/>
 
-let timesheetModule = angular.module("timesheetModule", ["angular-loading-bar", "ngRoute", "sticky"])
+let timesheetModule = angular.module("timesheetModule", ["angular-loading-bar", "ngRoute", "sticky", "ui.utils.masks"])
 
 timesheetModule.config(["$routeProvider", function($routeProvider: angular.route.IRouteProvider) {
 	$routeProvider.when("/timesheets/:start", {
